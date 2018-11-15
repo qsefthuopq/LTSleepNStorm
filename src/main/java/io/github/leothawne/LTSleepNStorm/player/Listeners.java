4330eff04a37aa399b8026a3919ac497bda623b3
leothawne <leothawne@gmail.com>
2018-11-15 00:56:24 -0200

    Finished new sleeping mechanics.

--- src/main/java/io/github/leothawne/LTSleepNStorm/player/Listeners.java
+++ src/main/java/io/github/leothawne/LTSleepNStorm/player/Listeners.java
@@ -3,80 +3,39 @@ package io.github.leothawne.LTSleepNStorm.player;
 import java.util.ArrayList;
 
 import org.bukkit.Bukkit;
-import org.bukkit.World;
 import org.bukkit.World.Environment;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerBedEnterEvent;
-
-import io.github.leothawne.LTSleepNStorm.LTSleepNStormLoader;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 public class Listeners implements Listener {
-	private LTSleepNStormLoader plugin;
 	private FileConfiguration configuration;
-	public Listeners(LTSleepNStormLoader plugin, FileConfiguration configuration) {
-		this.plugin = plugin;
+	public Listeners(FileConfiguration configuration) {
 		this.configuration = configuration;
 	}
-	private String getSleepMessage(int playersSleeping, int playersNeeded, int playersOnline) {
-		return "Dormindo: [" + playersSleeping + "/" + playersNeeded + "/" + playersOnline + "]";
-	}
 	@EventHandler
-	public void onPlayerSleep(PlayerBedEnterEvent event) {
+	public void onPlayerInteract(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
-		World world = player.getLocation().getWorld();
-		ArrayList<?> worldList = new ArrayList<>(configuration.getList("worlds"));
-		if(worldList.contains(world.getName())) {
-			if(world.getEnvironment() != Environment.NETHER && world.getEnvironment() != Environment.THE_END) {
-				int playersOnline = 0;
-				int playersSleeping = 0;
-				for(Player players : Bukkit.getOnlinePlayers()) {
-					if(players.getLocation().getWorld().getName().equalsIgnoreCase(world.getName())) {
-						playersOnline++;
-						if(players.isSleeping()) {
-							playersSleeping++;
-						}
-					}
-				}
-				Double playersNeeded1 = playersOnline * 0.5;
-				int playersNeeded2 = playersNeeded1.intValue();
-				int playersNeeded3 = 1;
-				if(playersNeeded2 > 0) {
-					playersNeeded3 = playersNeeded2;
-				}
-				if(world.getTime() < 12300 || world.getTime() > 23850) {
-					player.sendMessage(getSleepMessage(playersSleeping, playersNeeded3, playersOnline));
-				}
-				if(world.hasStorm()) {
-					player.sendMessage(getSleepMessage(playersSleeping, playersNeeded3, playersOnline));
-				}
-				if(world.isThundering()) {
-					player.sendMessage(getSleepMessage(playersSleeping, playersNeeded3, playersOnline));
-				}
-				if(playersSleeping >= playersNeeded3) {
-					if(world.getTime() < 12300 || world.getTime() > 23850) {
-						player.sendMessage(getSleepMessage(playersSleeping, playersNeeded3, playersOnline));
-						for(World worlds : Bukkit.getWorlds()) {
-							if(worlds.getName().equalsIgnoreCase(world.getName())) {
-								worlds.setTime(0);
+		if(player.hasPermission("LTSleepNStorm.use") && player.hasPermission("LTSleepNStorm.sleep")) {
+			if(event.getHand() == EquipmentSlot.HAND && event.getAction() == Action.RIGHT_CLICK_BLOCK && player.isSneaking() == false){
+				Block block = event.getClickedBlock();
+				if(block.getType().toString().contains("_BED")) {
+					ArrayList<?> worldList = new ArrayList<>(configuration.getList("worlds"));
+					if(worldList.contains(Bukkit.getWorld(player.getLocation().getWorld().getUID()).getName())) {
+						if(Bukkit.getWorld(player.getLocation().getWorld().getUID()).getEnvironment() != Environment.NETHER && Bukkit.getWorld(player.getLocation().getWorld().getUID()).getEnvironment() != Environment.THE_END) {
+							if(Bukkit.getWorld(player.getLocation().getWorld().getUID()).getTime() > 12300 && Bukkit.getWorld(player.getLocation().getWorld().getUID()).getTime() < 23850) {
+								Bukkit.getWorld(player.getLocation().getWorld().getUID()).setTime(0);
 							}
-						}
-					}
-					if(world.hasStorm()) {
-						player.sendMessage(getSleepMessage(playersSleeping, playersNeeded3, playersOnline));
-						for(World worlds : Bukkit.getWorlds()) {
-							if(worlds.getName().equalsIgnoreCase(world.getName())) {
-								worlds.setStorm(false);
+							if(Bukkit.getWorld(player.getLocation().getWorld().getUID()).hasStorm()) {
+								Bukkit.getWorld(player.getLocation().getWorld().getUID()).setStorm(false);
 							}
-						}
-					}
-					if(world.isThundering()) {
-						player.sendMessage(getSleepMessage(playersSleeping, playersNeeded3, playersOnline));
-						for(World worlds : Bukkit.getWorlds()) {
-							if(worlds.getName().equalsIgnoreCase(world.getName())) {
-								worlds.setThundering(false);
+							if(Bukkit.getWorld(player.getLocation().getWorld().getUID()).isThundering()) {
+								Bukkit.getWorld(player.getLocation().getWorld().getUID()).setThundering(false);
 							}
 						}
 					}
