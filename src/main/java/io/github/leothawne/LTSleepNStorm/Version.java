1958b5a357caf36094f5aca8d146a986305cbc08
Build Tools <31146142+leothawne@users.noreply.github.com>
2019-02-20 23:34:23 -0300

    Update Version.java

--- src/main/java/io/github/leothawne/LTSleepNStorm/Version.java
+++ src/main/java/io/github/leothawne/LTSleepNStorm/Version.java
@@ -19,8 +19,8 @@ public class Version {
 	private static final int configFileVersion = 1;
 	private static final int english_languageFileVersion = 2;
 	private static final int portuguese_languageFileVersion = 2;
-	private static final String LTSNSVersion = "0.2.0";
-	private static final String LTSNSVersion_Date = "99/99/9999 (America/Sao_Paulo)";
+	private static final String Plugin_Version = "0.2.0";
+	private static final String Plugin_Date = "99/99/9999 (America/Sao_Paulo)";
 	private static final String Minecraft_Version = "1.13.x";
 	private static final String Minecraft_Build = "1.13-R0.1-SNAPSHOT";
 	private static final String Java_Version = "8+";
@@ -36,10 +36,10 @@ public class Version {
 		return 0;
 	}
 	public static final String getVersionNumber() {
-		return LTSNSVersion;
+		return Plugin_Version;
 	}
 	public static final String getVersionDate() {
-		return LTSNSVersion_Date;
+		return Plugin_Date;
 	}
 	public static final String getMinecraftVersion() {
 		return Minecraft_Version;
@@ -51,11 +51,11 @@ public class Version {
 		return Java_Version;
 	}
 	public static final void version(CommandSender sender) {
-		sender.sendMessage(ChatColor.AQUA + "LT Sleep N Storm " + ChatColor.YELLOW + "plugin " + ChatColor.GREEN + "" + LTSNSVersion + "" + ChatColor.YELLOW + " (" + ChatColor.GREEN + "" + LTSNSVersion_Date + "" + ChatColor.YELLOW + "), Minecraft " + ChatColor.GREEN + "" + Minecraft_Version +  "" + ChatColor.YELLOW + " (Java " + ChatColor.GREEN + "" + Java_Version + "" + ChatColor.YELLOW + ", build " + ChatColor.GREEN + "" + Minecraft_Build + "" + ChatColor.YELLOW + ").");
+		sender.sendMessage(ChatColor.AQUA + "LT Sleep N Storm " + ChatColor.YELLOW + "plugin " + ChatColor.GREEN + "" + Plugin_Version + "" + ChatColor.YELLOW + " (" + ChatColor.GREEN + "" + Plugin_Date + "" + ChatColor.YELLOW + "), Minecraft " + ChatColor.GREEN + "" + Minecraft_Version +  "" + ChatColor.YELLOW + " (Java " + ChatColor.GREEN + "" + Java_Version + "" + ChatColor.YELLOW + ", build " + ChatColor.GREEN + "" + Minecraft_Build + "" + ChatColor.YELLOW + ").");
 	}
 	public static final void check() {
 		try {
-			URLConnection allowedUrl = new URL("https://leothawne.github.io/LTItemMail/api/" + LTSNSVersion + "/plugin.html").openConnection();
+			URLConnection allowedUrl = new URL("https://leothawne.github.io/LTItemMail/api/" + Plugin_Version + "/plugin.html").openConnection();
 			allowedUrl.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
 			allowedUrl.connect();
 			BufferedReader allowedReader = new BufferedReader(new InputStreamReader(allowedUrl.getInputStream(), Charset.forName("UTF-8")));
@@ -66,7 +66,7 @@ public class Version {
 			}
 			if(sb.toString() != null) {
 				if(sb.toString().equalsIgnoreCase("disabled")) {
-					myLogger.severe("Hey you, stop right there! The version " + LTSNSVersion + " is not allowed anymore!");
+					myLogger.severe("Hey you, stop right there! The version " + Plugin_Version + " is not allowed anymore!");
 					myLogger.severe("Apologies, but this plugin will now be disabled! Download a newer version to play: https://dev.bukkit.org/projects/lt-sleep-n-storm");
 					plugin.getServer().getPluginManager().disablePlugin(plugin);
 				}
